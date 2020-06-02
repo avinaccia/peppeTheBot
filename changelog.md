309c1c60373f30d8c2a1b2386388a129c1ce9baa
Alessandro Vinaccia <vinacciaalessandro@gmail.com>
2020-06-03 00:19:05 +0200

    Improved status change function

---
+++ changelog.md
@@ -0,0 +1,9 @@
+#Peppe the bot
+
+###02-06-2020
+-Frist commit!
+
+###03-06-2020
+-Fixed bug with the status update message function.
+-Removed case-sensitive commands
+-Added the channel id as a enviromental variable
--- index.js
+++ index.js
@@ -24,89 +24,91 @@ const answers = [
     'Enzogucci ha le risposte',
 ]
 
-function statusHasCanghed(member) {
+function statusHasChanged(member) {
     let bool;
 
-    if (usersChrono.length > 0) {
-        usersChrono.forEach((user) => {
-            if (member.id == user.id && member.presence.status == user.lastStatus) {
-                bool = false;
-            } else if (member.id == user.id) {
-                user.lastStatus = member.presence.status;
-                bool = true;
-            }
-        })
-    } else {
-        bool = true;
-    }
+    usersChrono.forEach((user) => {
+        if (member.id === user.id && member.presence.status == user.lastStatus) {
+            bool = false;
+        }
+    })
+    if (bool == undefined) bool = true;
     return bool;
 }
 
-
 client.once('ready', () => {
     console.log("Peppe is ready!")
-    channel = client.channels.cache.get('665184285858463754');
+    channel = client.channels.cache.get(process.env.BOT_CHANNEL);
+
+    client.guilds.cache.get("665181987077750834").members.fetch().then(members => {
+        members.forEach(member => {
+            let obj = {
+                id: member.id,
+                lastStatus: member.presence.status
+            };
+            usersChrono.push(obj);
+        })
+    })
 });
 
 client.on('presenceUpdate', (status) => {
     let message;
 
-    if (statusHasCanghed(status.member)) {
-
-        switch (status.member.presence.status) {
-            case "online":
-                message = "è qui  😍"
-                break
-            case "dnd":
-                message = "è un pò antipatico  🛑"
-                break
-            case "offline":
-                message = "non è più tra noi  🧟‍♂️"
-                break
-            case "idle":
-                message = "ha preso sonno  💤"
-                break
+    if (status != undefined) {
+        if (statusHasChanged(status.member)) {
+            switch (status.member.presence.status) {
+                case "online":
+                    message = "è qui  😍"
+                    break;
+                case "dnd":
+                    message = "è un pò antipatico  🛑"
+                    break;
+                case "offline":
+                    message = "non è più tra noi  🧟‍♂️"
+                    break;
+                case "idle":
+                    message = "ha preso sonno  💤"
+                    break;
+                default:
+                    message = "ha cambiato il suo stato! 🐱‍👤"
+                    break;
+            }
+            channel.send(`${status.member} ${message}`);
+            //only for debug
+            console.log(`${status.member} ha cambiato il suo stato in ${status.member.presence.status}`)
+            //
+            usersChrono[usersChrono.map((e) => { return e.id }).indexOf(status.member.id)].lastStatus = status.member.presence.status;
         }
-        channel.send(`${status.member} ${message}`)
     }
-
-    let x = usersChrono.find((obj) => obj.id == status.member.id);
-
-    if (x == undefined) {
-        usersChrono.push({ 'id': status.member.id, 'lastStatus': status.member.presence.status });
-    } else {
-        let index = usersChrono.map((e) => { return e.id }).indexOf(status.member.id);
-        usersChrono[index].lastStatus = status.member.presence.status;
-    }
-
 })
 
 client.on('message', (msg) => {
-    let message;
-
     if (msg.content.toLowerCase() == "!help") {
         channel.send(`Ciao ${msg.author.username}! Sono Peppe e rendo questo server più interessante! Per ora i miei comandi sono : !watch, !poke @<nome>, !8Ball <domanda>. Provali per scoprire cosa fanno!`);
+    }
+    else if (msg.content.toLowerCase().search("sile") != -1) {
+        channel.send('Non capisco perchè tutti mi odiate :/');
     }
     else if (msg.content.toLowerCase() == 'ping') {
         channel.send("pong!");
     }
     else if (msg.content.toLowerCase() == "!watch") {
-        channel.send("Ecco la stanza https://bit.ly/2Avtbqu !");
+        channel.send(`Ecco la stanza ${process.env.WATCH2GETHER_URL} !`);
     }
     else if (msg.content.toLowerCase() == "omg") {
         channel.send("omg omg");
     }
-    else if (msg.content.substring(0, 5) == "!poke" && msg.content.length > 5) {
-        let user = msg.mentions.users.first(); //grabbing the user mention
-
+    else if (msg.content.substring(0, 5).toLowerCase() == "!poke" && msg.content.length > 5) {
         for (let i = 0; i < 5; i++) {
-            user.send(`Sveglia! ${msg.author.username} vuole giocare con te!`);
+            msg.mentions.users.first().send(`Sveglia! ${msg.author.username} vuole giocare con te!`);
         }
+        channel.send(`${user.username} è stato avvisato!`)
     }
-    else if (msg.content.substring(0, 6) == "!8Ball" && msg.content.length > 5) {
+    else if (msg.content.substring(0, 6).toLowerCase() == "!8ball".toLowerCase() && msg.content.length > 5) {
         let reply = Math.floor(Math.random() * answers.length)
         channel.send(answers[reply]);
-    } else if (msg.content.charAt(0) == "!") {
+    }
+    else if (msg.content.charAt(0) == "!") {
         channel.send("Mhhh, non ho alcun comando con quel nome...")
     }
 })
